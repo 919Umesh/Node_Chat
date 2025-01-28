cd1b271ef90adba6db7241740edf93eefecf80fd
Umesh <globaltechumesh11@gmail.com>
2025-01-28 17:24:26 +0545

    Multer

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 const { handleCreateUser, handleGetAllUsers, handleLoginUser } = require('../controllers/login');
 const { authenticateToken } = require('../middlewares/authorization');
 
