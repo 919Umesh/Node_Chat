c34b2781d3f3391c179898e2878519fd6f07e942
Umesh <globaltechumesh11@gmail.com>
2025-01-28 13:07:55 +0545

    Routes

--- routes/login.js
+++ routes/login.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { handleCreateUser, handleGetAllUsers, handleLoginUser } = require('../controllers/login');
 const { authenticateToken } = require('../middlewares/authorization');
 
-router.post('/createUser', authenticateToken, handleCreateUser);
-router.get('/getUsers', authenticateToken, handleGetAllUsers); 
+router.post('/createUser', handleCreateUser);
+router.get('/getUsers', handleGetAllUsers); 
 router.post('/loginUser', handleLoginUser); 
 
 module.exports = router;
