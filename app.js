dfb9ae8c47e8ad1c3c89874924dabe98f748fca3
Umesh <globaltechumesh11@gmail.com>
2025-01-30 17:43:47 +0545

    Clear

--- app.js
+++ app.js
@@ -3,16 +3,13 @@ const path = require('path');
 const { connectMongoDB } = require('./connection');
 const userRouter = require('./routes/login');
 const notesRouter = require('./routes/notes');
-const questionRouter = require('./routes/questions');
 const app = express();
 const port =  4000;
 
 app.use(express.json());
 
-//Stored folder location
 app.use('/userProfile', express.static('userProfile'));
 app.use('/notes', express.static('notes'));
-app.use('/questions', express.static('questions'));
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -20,7 +17,6 @@ connectMongoDB('mongodb+srv://globaltechumesh11:E.ecAk7t.2UUuyK@projectmanage.an
 
 app.use('/users', userRouter);
 app.use('/notes', notesRouter);
-app.use('/questions', questionRouter);
 
 app.listen(port, () => {
   console.log(`Server is running in https://node-chat-mvlu.onrender.com:${port}`);
