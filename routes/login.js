51bfc43f5c4ab23b35e29d95e577cbbab51f6ecc
Umesh <globaltechumesh11@gmail.com>
2025-01-31 12:49:26 +0545

    Profile

--- routes/login.js
+++ routes/login.js
@@ -8,14 +8,8 @@ const storage = multer.diskStorage({
     destination: (req, file, cb) => cb(null, 'userProfile/'),
     filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`)
 });
-//const upload = multer({ storage });
-const upload = multer({ 
-    storage: storage,
-    fileFilter: fileFilter,
-    limits: {
-        fileSize: 10 * 1024 * 1024 
-    }
-});
+
+const upload = multer({ storage });
 
 router.post('/createUser',upload.single('profileImage'), handleCreateUser);
 router.get('/getUsers', handleGetAllUsers); 
